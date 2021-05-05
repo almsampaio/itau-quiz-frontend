9db09d32279f35d92ab716b798a7d76ae9f76d7b
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-05 10:20:38 -0300

    feat: create a component Loading for a more pleasant user experience during api requests

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import loadingOrange from '../../assets/loadingOrange.svg';
+import loadingWhite from '../../assets/loadingWhite.svg';
+
+import { Container } from './styles';
+
+interface LoadingProps {
+  color: 'white' | 'orange';
+}
+
+export function Loading({color} : LoadingProps) : JSX.Element {
+  return (
+    <Container>
+      {color === 'white'
+      ? <img src={loadingWhite} alt="Carregando"/>
+      : <img src={loadingOrange} alt="Carregando"/>}
+    </Container>
+  );
+}
