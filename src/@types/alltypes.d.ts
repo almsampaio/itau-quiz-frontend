48f511ce0b59aa178fa5db612c8a5389e587551b
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-18 12:05:54 -0300

    fix: include react-router-dom module into @types

--- src/@types/alltypes.d.ts
+++ src/@types/alltypes.d.ts
@@ -1,7 +1,8 @@
-declare module 'react-router-dom';
-
 import { AriaAttributes, DOMAttributes } from 'react';
 import { RegisterOptions } from 'react-hook-form';
+
+declare module 'react-router-dom';
+
 declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
     register?: RegisterOptions;
