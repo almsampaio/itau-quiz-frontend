1bf79437bbf9cf29afa7a90c94f0bd40e523a635
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-19 15:25:32 -0300

    refactor: create authenticated and unauthenticated instances of axios with interceptors

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,57 @@
 import axios from 'axios';
 
-export const api = axios.create({
+const config = {
   baseURL: process.env.REACT_APP_BASE_URL,
-});
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+export const authenticatedInstance = axios.create(config);
+export const unauthenticatedInstance = axios.create(config);
+
+export default {
+  unauthorized() {
+    unauthenticatedInstance.interceptors.request.use(
+      (response) => response,
+      (error) => error,
+    );
+
+    return unauthenticatedInstance;
+  },
+
+  authorized() {
+    authenticatedInstance.interceptors.request.use(
+      async (config) => {
+        config.headers = {
+          Authorization: `Bearer ${localStorage.getItem('auth')}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        };
+        return config;
+      },
+      (error) => {
+        Promise.reject(error);
+      },
+    );
+
+    authenticatedInstance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        //   const originalRequest = error.config;
+        //   if (error.response.status === 403 && !originalRequest._retry) {
+        //     originalRequest._retry = true;
+        //     const access_token = await refreshAccessToken();
+        //     axios.defaults.headers.common[
+        //       'Authorization'
+        //     ] = `Bearer ${access_token}`;
+        //     localStorage.setItem('auth', access_token.data);
+        //     return authenticatedInstance(originalRequest);
+        //   }
+        return Promise.reject(error);
+      },
+    );
+
+    return authenticatedInstance;
+  },
+};
