bec71716408a8224105a29e0d157117c836c4149
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-21 08:55:34 -0300

    refactor(QuizDownload): improve readability of onSubmit function

--- src/pages/QuizDownload/QuizDownload.tsx
+++ src/pages/QuizDownload/QuizDownload.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { AxiosResponse } from 'axios';
 
 import { getQuizFile } from 'api/quiz';
 import closeImg from 'assets/close.svg';
@@ -18,21 +19,25 @@ export default function QuizDownload(): JSX.Element {
   async function downloadQuiz() {
     setIsLoading(true);
     try {
-      const { data } = await getQuizFile(id);
-      const url = window.URL.createObjectURL(new Blob([data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute(
-        'download',
-        `${id.padStart(2, '0')}._Quiz_Fato_ou_Fake.zip`,
-      );
-      document.body.appendChild(link);
-      link.click();
+      const response = await getQuizFile(id);
+      createUrlAndFireDownload(response);
       setIsLoading(false);
     } catch (error) {
       setIsLoading(false);
       console.log(error);
     }
+  }
+
+  function createUrlAndFireDownload({ data }: AxiosResponse) {
+    const url = window.URL.createObjectURL(new Blob([data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute(
+      'download',
+      `${id.padStart(2, '0')}._Quiz_Fato_ou_Fake.zip`,
+    );
+    document.body.appendChild(link);
+    link.click();
   }
 
   return (
