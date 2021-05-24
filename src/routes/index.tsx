e45b7fe2ca688cbe93bc1aea9993fe6ef25ae6b6
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-24 09:22:59 -0300

    feat: wrap the Switch inside a React.Suspense with fallback on Loading component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,35 @@
+import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { useAuth } from 'contexts/AuthContext';
 import { PrivateRoutes } from 'routes/PrivateRoutes';
 import { PublicRoutes } from 'routes/PublicRoutes';
 
+import Loading from 'components/common/Loading';
+
 export function Routes() {
   const { auth } = useAuth();
   return (
-    <Switch>
-      {PublicRoutes.map((publicRoute) => publicRoute)}
-      {PrivateRoutes.map((privateRoute) => privateRoute)}
+    <React.Suspense fallback={<Loading color="white" />}>
+      <Switch>
+        {PublicRoutes.map((publicRoute) => publicRoute)}
+        {PrivateRoutes.map((privateRoute) => privateRoute)}
 
-      <Route
-        exact
-        path="/"
-        render={() => {
-          return auth.token ? (
-            <Redirect to="/quiz-form" />
-          ) : (
-            <Redirect to="/login" />
-          );
-        }}
-      />
-      <Route>
-        <Redirect to="/" />
-      </Route>
-    </Switch>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return auth.token ? (
+              <Redirect to="/quiz-form" />
+            ) : (
+              <Redirect to="/login" />
+            );
+          }}
+        />
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </React.Suspense>
   );
 }
