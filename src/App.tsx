777d0c2610fe170a5a9c44585a99a3b59ef48009
Andre Sampaio <andresampaiolm@gmail.com>
2021-05-24 10:06:29 -0300

    refactor: remove auth.rehydrated check from App to routes/index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useAuth } from 'contexts/AuthContext';
 import { Routes } from 'routes';
 
-import Loading from 'components/common/Loading';
-
 function App() {
-  const { auth } = useAuth();
-
-  return <>{auth.rehydrated ? <Routes /> : <Loading color="white" />}</>;
+  return <Routes />;
 }
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,26 +10,32 @@ import Loading from 'components/common/Loading';
 export function Routes() {
   const { auth } = useAuth();
   return (
-    <React.Suspense fallback={<Loading color="white" />}>
-      <Switch>
-        {PublicRoutes.map((publicRoute) => publicRoute)}
-        {PrivateRoutes.map((privateRoute) => privateRoute)}
+    <>
+      {auth.rehydrated ? (
+        <React.Suspense fallback={<Loading color="white" />}>
+          <Switch>
+            {PublicRoutes.map((publicRoute) => publicRoute)}
+            {PrivateRoutes.map((privateRoute) => privateRoute)}
 
-        <Route
-          exact
-          path="/"
-          render={() => {
-            return auth.token ? (
-              <Redirect to="/quiz-form" />
-            ) : (
-              <Redirect to="/login" />
-            );
-          }}
-        />
-        <Route>
-          <Redirect to="/" />
-        </Route>
-      </Switch>
-    </React.Suspense>
+            <Route
+              exact
+              path="/"
+              render={() => {
+                return auth.token ? (
+                  <Redirect to="/quiz-form" />
+                ) : (
+                  <Redirect to="/login" />
+                );
+              }}
+            />
+            <Route>
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </React.Suspense>
+      ) : (
+        <Loading color="white" />
+      )}
+    </>
   );
 }
